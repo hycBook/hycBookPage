4364c19f946ccb9ff1abfa0c491fa4db0bed7fe0
hycBook <1832044043@qq.com>
2023-10-28 10:23:40 +0000

    Update from https://github.com/hycBook/hycBlog/commit/3fe1a81b2b0bae25a8ed39401526e211ae5630f4

--- js/udf_js.js
+++ js/udf_js.js
@@ -45,6 +45,19 @@ function switchNightMode() {
 }
 
 
+document.addEventListener('keydown', function(event) {
+    if (event.ctrlKey && event.key === 'k') {
+        // 当按下 Ctrl+K 时执行搜索操作
+        event.preventDefault(); // 防止浏览器默认行为（可能会触发浏览器的搜索）
+        //search(); // 调用执行搜索的函数
+        var divElement = document.getElementById('search-button');
+        var linkElement = divElement.querySelector('a');
+        linkElement.click(); 
+    }
+});
+
+        
+
 /* 自定义弹窗  https://moonshuo.cn/posts/25274.html#自带的弹窗 */
 document.addEventListener("copy", (function (e) {
     toastr.options = {
