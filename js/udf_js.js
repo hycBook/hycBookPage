38aa5cb3d0c21c00adba130492e857c8f5f82c27
hycBook <1832044043@qq.com>
2023-10-29 00:08:24 +0000

    Update from https://github.com/hycBook/hycBlog/commit/24009cd54166c439b2e3f99fb33a35e966ac1dbc

--- js/udf_js.js
+++ js/udf_js.js
@@ -108,26 +108,6 @@ document.addEventListener("copy", (function (e) {
 }));
 
 
-// https://sscarf.com/posts/17168.html#时间轴添加生肖
-hexo.extend.helper.register('getAnimalIcon', function (year) {
-    var index = parseInt(year) % 12;
-    var icon = {
-        0: 'icon-monkey',
-        1: 'icon-rooster',
-        2: 'icon-dog',
-        3: 'icon-boar',
-        4: 'icon-rat',
-        5: 'icon-ox',
-        6: 'icon-tiger',
-        7: 'icon-rabbit',
-        8: 'icon-dragon',
-        9: 'icon-snake',
-        10: 'icon-horse',
-        11: 'icon-goat',
-    }
-    return icon[index]
-});
-
 // 作者: Justlovesmile
 // 链接: https://blog.justlovesmile.top/posts/2bfb1caa.html?time=1694779141418
 // 来源: Justlovesmile's BLOG
